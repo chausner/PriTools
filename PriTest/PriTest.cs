9dbcf630cd8c580be32c62aa0b08137af2fefb6e
Christoph Hausner <christoph_hausner@hotmail.de>
2025-12-03 20:39:03 +0100

    Use file-scoped namespace

--- PriTest/PriTest.cs
+++ PriTest/PriTest.cs
@@ -1,62 +1,61 @@
 ﻿using PriFormat;
 using System.Diagnostics;
 
-namespace PriTest
+namespace PriTest;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        if (args.Length == 0)
         {
-            if (args.Length == 0)
+            Console.WriteLine("PriTest <one or more paths to .pri files or paths to folders containing .pri files>");
+            return;
+        }
+
+        List<string> paths = [];
+
+        foreach (string path in args)            
+            if (File.Exists(path))
+                paths.Add(path);
+            else if (Directory.Exists(path))
+                paths.AddRange(Directory.GetFiles(path, "*.pri", SearchOption.AllDirectories));
+            else
+                throw new Exception(string.Format("Path does not exist: {0}", path));
+
+        List<(string Path, string Exception)> errors = [];
+
+        foreach (string path in paths)
+        {
+            try
             {
-                Console.WriteLine("PriTest <one or more paths to .pri files or paths to folders containing .pri files>");
-                return;
+                using Stream stream = File.OpenRead(path);
+                PriFile priFile = PriFile.Parse(stream);
+
+                var unknownSections = priFile.Sections.OfType<UnknownSection>();
+
+                if (unknownSections.Any())
+                    throw new Exception(string.Format("Unknown sections: {0}", string.Join(' ', unknownSections.Select(s => s.SectionIdentifier))));
             }
-
-            List<string> paths = [];
-
-            foreach (string path in args)            
-                if (File.Exists(path))
-                    paths.Add(path);
-                else if (Directory.Exists(path))
-                    paths.AddRange(Directory.GetFiles(path, "*.pri", SearchOption.AllDirectories));
-                else
-                    throw new Exception(string.Format("Path does not exist: {0}", path));
-
-            List<(string Path, string Exception)> errors = [];
-
-            foreach (string path in paths)
+            catch (Exception e) when (!Debugger.IsAttached)
             {
-                try
-                {
-                    using Stream stream = File.OpenRead(path);
-                    PriFile priFile = PriFile.Parse(stream);
-
-                    var unknownSections = priFile.Sections.OfType<UnknownSection>();
-
-                    if (unknownSections.Any())
-                        throw new Exception(string.Format("Unknown sections: {0}", string.Join(' ', unknownSections.Select(s => s.SectionIdentifier))));
-                }
-                catch (Exception e) when (!Debugger.IsAttached)
-                {
-                    Console.WriteLine("Error: {0}", path);
-                    Console.WriteLine(e);
-                    errors.Add((path, e.ToString()));
-                }
+                Console.WriteLine("Error: {0}", path);
+                Console.WriteLine(e);
+                errors.Add((path, e.ToString()));
             }
+        }
 
-            Console.WriteLine("Done. {0} files loaded successfully. {1} files failed to load.", paths.Count - errors.Count, errors.Count);
-            Console.WriteLine("Errors:");
+        Console.WriteLine("Done. {0} files loaded successfully. {1} files failed to load.", paths.Count - errors.Count, errors.Count);
+        Console.WriteLine("Errors:");
 
-            foreach (var errorGroup in errors.GroupBy(x => x.Exception).OrderByDescending(g => g.Count()).ThenBy(g => g.Key))
-            {
-                Console.WriteLine("{0}:", errorGroup.Key);
+        foreach (var errorGroup in errors.GroupBy(x => x.Exception).OrderByDescending(g => g.Count()).ThenBy(g => g.Key))
+        {
+            Console.WriteLine("{0}:", errorGroup.Key);
 
-                foreach (var item in errorGroup.OrderBy(x => x.Path))                
-                    Console.WriteLine("  {0}", item.Path);
+            foreach (var item in errorGroup.OrderBy(x => x.Path))                
+                Console.WriteLine("  {0}", item.Path);
 
-                Console.WriteLine();
-            }
+            Console.WriteLine();
         }
     }
 }
