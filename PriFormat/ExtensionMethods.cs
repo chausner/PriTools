55e5755c22c4cb2aac75d7a74f02593657f1bf82
Christoph Hausner <christoph_hausner@hotmail.de>
2026-07-16 23:14:09 +0200

    Refactor extension methods

--- PriFormat/ExtensionMethods.cs
+++ PriFormat/ExtensionMethods.cs
@@ -5,45 +5,48 @@ namespace PriFormat;
 
 internal static class ExtensionMethods
 {
-    public static string ReadString(this BinaryReader reader, Encoding encoding, int length)
+    extension(BinaryReader reader)
     {
-        using BinaryReader r = new(reader.BaseStream, encoding, true);
+        public string ReadString(Encoding encoding, int length)
+        {
+            using BinaryReader r = new(reader.BaseStream, encoding, true);
 
-        return new string(r.ReadChars(length));
-    }
+            return new string(r.ReadChars(length));
+        }
 
-    public static string ReadNullTerminatedString(this BinaryReader reader, Encoding encoding)
-    {
-        using BinaryReader r = new(reader.BaseStream, encoding, true);
+        public string ReadNullTerminatedString(Encoding encoding)
+        {
+            using BinaryReader r = new(reader.BaseStream, encoding, true);
 
-        StringBuilder result = new();
-        char c;
-        while ((c = r.ReadChar()) != '\0')
-            result.Append(c);
-        return result.ToString();
-    }
+            StringBuilder result = new();
+            char c;
+            while ((c = r.ReadChar()) != '\0')
+                result.Append(c);
+            return result.ToString();
+        }
 
-    public static void ExpectByte(this BinaryReader reader, byte expectedValue)
-    {
-        if (reader.ReadByte() != expectedValue)
-            throw new InvalidDataException("Unexpected value read.");
-    }
+        public void ExpectByte(byte expectedValue)
+        {
+            if (reader.ReadByte() != expectedValue)
+                throw new InvalidDataException("Unexpected value read.");
+        }
 
-    public static void ExpectUInt16(this BinaryReader reader, ushort expectedValue)
-    {
-        if (reader.ReadUInt16() != expectedValue)
-            throw new InvalidDataException("Unexpected value read.");
-    }
+        public void ExpectUInt16(ushort expectedValue)
+        {
+            if (reader.ReadUInt16() != expectedValue)
+                throw new InvalidDataException("Unexpected value read.");
+        }
 
-    public static void ExpectUInt32(this BinaryReader reader, uint expectedValue)
-    {
-        if (reader.ReadUInt32() != expectedValue)
-            throw new InvalidDataException("Unexpected value read.");
-    }
+        public void ExpectUInt32(uint expectedValue)
+        {
+            if (reader.ReadUInt32() != expectedValue)
+                throw new InvalidDataException("Unexpected value read.");
+        }
 
-    public static void ExpectString(this BinaryReader reader, string s)
-    {
-        if (new string(reader.ReadChars(s.Length)) != s)
-            throw new InvalidDataException("Unexpected value read.");
+        public void ExpectString(string s)
+        {
+            if (new string(reader.ReadChars(s.Length)) != s)
+                throw new InvalidDataException("Unexpected value read.");
+        }
     }
 }
